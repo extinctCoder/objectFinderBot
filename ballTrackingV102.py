f90cbde679260f51175d27d1195ed38a5b3f6b0c
darthVader <extinctCoder@outlook.com>
2019-03-17 02:05:37 +0600

    mudxx installed

--- ballTrackingV102.py
+++ ballTrackingV102.py
@@ -1,4 +1,6 @@
-from img import module_img_ai
+# from img import module_img_ai
 
-if __name__ == "__main__":
-    module_img_ai("extinctCoder", 0, True)
+# if __name__ == "__main__":
+#     module_img_ai("extinctCoder", 0, True)
+
+print ("hi")
